42090594c98fa120b90a964a943bc2a76c81fc31
Itsbat Husnul Iman <itsbathusnul@gmail.com>
2019-05-20 13:09:01 +0700

    Updates

---
+++ precache-manifest.8175eb5b8f03f1568d4ff8908c2060b3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "047ca133fc888cd5af3df52e6315163f",
+    "url": "/martahayu/index.html"
+  },
+  {
+    "revision": "1addeb168fc3ea823a85",
+    "url": "/martahayu/static/js/2.a0413a49.chunk.js"
+  },
+  {
+    "revision": "c9704ad7ad4df6ef398b",
+    "url": "/martahayu/static/js/main.100c8ef0.chunk.js"
+  },
+  {
+    "revision": "3d07760b000a9858fdaf",
+    "url": "/martahayu/static/js/runtime~main.eb45fed7.js"
+  }
+]);
